14a01f4d3b94568d3cbe884fab056ab38089565b
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-10-10 14:50:31 +0300

    feat: SIM-10 Добавлены моки для задач

--- src/core/types.ts
+++ src/core/types.ts
@@ -4,3 +4,19 @@ export type KanbanUser = {
   avatarUrl?: string;
   role: string;
 };
+
+export type KanbanTaskPriority = "low" | "medium" | "high" | "critical";
+
+export type KanbanTask = {
+  id: number;
+  name: string;
+  createdBy: number;
+  assignedTo: number[];
+  description: string;
+  deadline: string;
+  priority: KanbanTaskPriority;
+  createdAt: string;
+  editedAt: string;
+  comments: number;
+  labels: string[];
+};
