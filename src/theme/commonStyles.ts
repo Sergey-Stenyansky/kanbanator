38e7788c8ce7622d330d4f32e5ac58311c3db0ac
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-10-10 16:39:55 +0300

    feat: SIM-11 Добавлена карточка для задачи

---
+++ src/theme/commonStyles.ts
@@ -0,0 +1,11 @@
+export const flexSpaceBetween = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+};
+
+export const flexJcStart = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "start",
+};
