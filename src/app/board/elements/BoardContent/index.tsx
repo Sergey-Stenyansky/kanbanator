2a8462634ac3e4ee50f7bed2767a49ed2da32924
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-10-11 14:38:01 +0300

    feat: SIM-12 Макет для draggable элементов доски

--- src/app/board/elements/BoardContent/index.tsx
+++ src/app/board/elements/BoardContent/index.tsx
@@ -3,6 +3,11 @@ import { Box, Stack, Typography } from "@mui/material";
 import KanbanTaskCard from "@/components/KanbanTaskCard";
 
 import Spacing from "@/primitives/Spacing";
+import {
+  Droppable,
+  DroppableProvided,
+  DroppableStateSnapshot,
+} from "@hello-pangea/dnd";
 
 export interface BoardContentProps {
   flow: KanbanFlowItem | null;
@@ -29,11 +34,29 @@ const BoardColumn = ({ item }: { item: KanbanColumnItem }) => {
       <Spacing v={1} />
       <Typography variant="h6">{item.name}</Typography>
       <Spacing v={2} />
-      <Stack ml={1} mb={1} useFlexGap gap={1}>
-        {item.tasks.map((task) => (
-          <KanbanTaskCard key={task.id} task={task} />
-        ))}
-      </Stack>
+      <Droppable droppableId={"column-" + item.id}>
+        {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
+          <Stack
+            ref={provided.innerRef}
+            sx={{
+              backgroundColor: snapshot.isDraggingOver
+                ? "lightgreen"
+                : "lightblue",
+              height: "100%",
+            }}
+            ml={1}
+            mb={1}
+            p={1}
+            gap={1}
+            useFlexGap
+            {...provided.droppableProps}
+          >
+            {item.tasks.map((task, index) => (
+              <KanbanTaskCard index={index} key={task.id} task={task} />
+            ))}
+          </Stack>
+        )}
+      </Droppable>
     </Box>
   );
 };
--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,10 +1,86 @@
+"use client";
+
 import { Stack } from "@mui/material";
 import BoardContent from "./elements/BoardContent";
 
+import { DragDropContext, DropResult } from "@hello-pangea/dnd";
+import { KanbanFlowItem } from "@/core/types";
+
+const flow: KanbanFlowItem = {
+  id: 1,
+  name: "Kanban Board",
+  columns: [
+    {
+      id: 1,
+      name: "Backlog",
+      tasks: [
+        {
+          id: 1,
+          name: "Fix Header",
+          createdAt: "2024-10-10 14:00",
+          editedAt: "2024-10-10 14:00",
+          deadline: "2024-10-10 16:00",
+          createdBy: {
+            id: 1,
+            name: "Ava Parry",
+            role: "Developer",
+            avatarUrl: "https://i.pravatar.cc/300",
+          },
+          assignedTo: [
+            {
+              id: 1,
+              name: "Ava Parry",
+              role: "Developer",
+              avatarUrl: "https://i.pravatar.cc/300",
+            },
+          ],
+          priority: "low",
+          description: "test",
+          comments: 0,
+          labels: ["blocker", "bug"],
+        },
+        {
+          id: 2,
+          name: "Fix Description",
+          createdAt: "2024-10-10 14:00",
+          editedAt: "2024-10-10 14:00",
+          deadline: "2024-10-10 16:00",
+          createdBy: {
+            id: 1,
+            name: "Ava Parry",
+            role: "Developer",
+            avatarUrl: "https://i.pravatar.cc/300",
+          },
+          assignedTo: [
+            {
+              id: 1,
+              name: "Ava Parry",
+              role: "Developer",
+              avatarUrl: "https://i.pravatar.cc/300",
+            },
+          ],
+          priority: "medium",
+          description: "test",
+          comments: 0,
+          labels: ["financial risk", "bug"],
+        },
+      ],
+    },
+    { id: 2, name: "In progress", tasks: [] },
+    { id: 3, name: "Done", tasks: [] },
+  ],
+};
+
 export default function Board() {
+  const onDragEnd = (res: DropResult) => {
+    console.log(res);
+  };
+
   return (
-    <Stack>
-      <BoardContent flow={null} />
-    </Stack>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Stack>
+        <BoardContent flow={flow} />
+      </Stack>
+    </DragDropContext>
   );
 }
