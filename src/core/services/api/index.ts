ffd56f9972cfa7dc7b0edc8c949b07523515d2b8
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-10-16 15:33:28 +0300

    fix: добавлен параметр no cache для работы при деплое

--- src/core/services/api/index.ts
+++ src/core/services/api/index.ts
@@ -1,25 +1,37 @@
 import { getApiBaseUrl } from "@/helpers/getApiBaseUrl";
 
 export const getUsers = async () => {
-  const res = await fetch(getApiBaseUrl() + "/api/users", { method: "GET" });
+  const res = await fetch(getApiBaseUrl() + "/api/users", {
+    method: "GET",
+    cache: "no-store",
+  });
   const { data } = await res.json();
   return data;
 };
 
 export const getComments = async () => {
-  const res = await fetch(getApiBaseUrl() + "/api/comments", { method: "GET" });
+  const res = await fetch(getApiBaseUrl() + "/api/comments", {
+    method: "GET",
+    cache: "no-store",
+  });
   const { data } = await res.json();
   return data;
 };
 
 export const getTasks = async () => {
-  const res = await fetch(getApiBaseUrl() + "/api/tasks", { method: "GET" });
+  const res = await fetch(getApiBaseUrl() + "/api/tasks", {
+    method: "GET",
+    cache: "no-store",
+  });
   const { data } = await res.json();
   return data;
 };
 
 export const getColumns = async () => {
-  const res = await fetch(getApiBaseUrl() + "/api/columns", { method: "GET" });
+  const res = await fetch(getApiBaseUrl() + "/api/columns", {
+    method: "GET",
+    cache: "no-store",
+  });
   const { data } = await res.json();
   return data;
 };
